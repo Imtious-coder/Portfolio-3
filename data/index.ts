6f24207bef6fa47523b17ad9e6577fdd0f9da71f
Imtious-Coder <iarafat386@gmail.com>
2024-07-17 14:58:46 +0600

    :bug: UI space bug resolved.

--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,8 @@
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+  { name: "Home", link: "/" },
+  { name: "Projects", link: "/projects" },
+  { name: "Articles", link: "/articles" },
+  { name: "About", link: "/about" },
 ];
 
 export const gridItems = [
